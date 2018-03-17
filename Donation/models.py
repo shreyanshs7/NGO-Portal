9e8e98447dc10f1a6f686c9fbd1efb56a8e6ed96
shreyanshs7 <shreyanshss7@gmail.com>
2018-03-17 11:40:04 +0530

    Added Donation models

--- Donation/models.py
+++ Donation/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from User_Login_Register.models import UserDetail
 from django.db import models
-
+from NGO_Items.models import Item
 # Create your models here.
+class Donation(models.Model):
+    user = models.ForeignKey("User_Login_Register.Userdetail")
+    item = models.ForeignKey("NGO_Items.Item")
+    quantity = models.IntegerField()
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+    details = models.CharField(max_length=1000)
